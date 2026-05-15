d484e417aa6082589dbdc5aa5f9a2feaf0fef59b
MrHua269 <mrhua269@gmail.com>
2026-05-15 17:57:10 +0800

    删除虚拟玩家api支持

--- Freesia-Backend/src/main/java/meow/kikir/freesia/backend/FreesiaBackend.java
+++ Freesia-Backend/src/main/java/meow/kikir/freesia/backend/FreesiaBackend.java
@@ -1,6 +1,5 @@
 package meow.kikir.freesia.backend;
 
-import meow.kikir.freesia.backend.misc.VirtualPlayerManager;
 import meow.kikir.freesia.backend.tracker.TrackerProcessor;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -9,7 +8,7 @@ public final class FreesiaBackend extends JavaPlugin {
     public static FreesiaBackend INSTANCE;
 
     private final TrackerProcessor trackerProcessor = new TrackerProcessor();
-    private final VirtualPlayerManager virtualPlayerManager = new VirtualPlayerManager();
+
 
     @Override
     public void onEnable() {
@@ -19,17 +18,10 @@ public final class FreesiaBackend extends JavaPlugin {
         Bukkit.getMessenger().registerIncomingPluginChannel(this, TrackerProcessor.CHANNEL_NAME, this.trackerProcessor);
         Bukkit.getMessenger().registerOutgoingPluginChannel(this, TrackerProcessor.CHANNEL_NAME);
 
-        // TODO- De-hard-coding?
-        Bukkit.getMessenger().registerIncomingPluginChannel(this, VirtualPlayerManager.CHANNEL_NAME, this.virtualPlayerManager);
-        Bukkit.getMessenger().registerOutgoingPluginChannel(this, VirtualPlayerManager.CHANNEL_NAME);
-
 
         Bukkit.getPluginManager().registerEvents(this.trackerProcessor, this);
     }
 
-    public VirtualPlayerManager getVirtualPlayerManager() {
-        return this.virtualPlayerManager;
-    }
 
     @Override
     public void onDisable() {
